8a3d88c4a1adc6deff1c2220edafd6945b5738b1
Dan Menza <dan.menza@gpshopper.com>
2019-05-07 13:33:21 -0500

    initial commit

--- blpInnovationProject/ViewControllers/LoginViewController.swift
+++ blpInnovationProject/ViewControllers/LoginViewController.swift
@@ -12,6 +12,8 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // adding new viewController
 
         // Do any additional setup after loading the view.
     }
